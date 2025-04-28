2734e479eb5ddee82c09d6b2fe5275019f1a5799
Sue Raisty <sraisty@gmail.com>
2025-04-28 01:38:48 -0700

    add polygonSlice and reducers

---
+++ store/polygonSlice.ts
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import { LatLngExpression } from "leaflet";
+
+// Define a type for the slice state
+interface PolygonState {
+  unsavedPolygon: LatLngExpression[];
+  savedPolygons: LatLngExpression[][];
+}
+
+// Define the initial state using that type
+const initialState: PolygonState = {
+  unsavedPolygon: [],
+  savedPolygons: [],
+};
+
+export const polygonsSlice = createSlice({
+  name: "polygons",
+
+  initialState,
+  reducers: {
+    addPoint: (state, action: PayloadAction<LatLngExpression>) => {
+      console.log("addPoint reducer");
+      state.unsavedPolygon.push(action.payload);
+    },
+    savePolygon: (state) => {
+      console.log("savePolygon reducer");
+      state.savedPolygons.push(state.unsavedPolygon);
+      state.unsavedPolygon = [];
+    },
+    // Use the PayloadAction type to declare the contents of `action.payload`
+    resetPolygons: (state) => {
+      console.log("resetPolygons reducer");
+      state.unsavedPolygon = [];
+      state.savedPolygons = [];
+    },
+  },
+});
+
+export const { addPoint, savePolygon, resetPolygons } = polygonsSlice.actions;
+
+export default polygonsSlice.reducer;
--- store/store.ts
+++ store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import appReducer from "./appSlice";
+import polygonsReducer from "./polygonSlice";
 
 export const store = configureStore({
   reducer: {
     app: appReducer,
+    polygons: polygonsReducer,
   },
 });
 
