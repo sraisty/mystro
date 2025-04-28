9878e12e51b939ad2ef1078cb57c2fb465a91d5e
Sue Raisty <sraisty@gmail.com>
2025-04-28 00:30:58 -0700

    use emotion styles

--- components/MyMap.tsx
+++ components/MyMap.tsx
@@ -1,5 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "leaflet/dist/leaflet.css";
+import type * as ReactLeaflet from "react-leaflet";
 
-export const MyMap = () => {
-  return <p>This is a map component.</p>;
+type Leaflet = {
+  MapContainer: typeof ReactLeaflet.MapContainer;
+  TileLayer: typeof ReactLeaflet.TileLayer;
+  Circle: typeof ReactLeaflet.Circle;
+  Marker: typeof ReactLeaflet.Marker;
+  Polygon: typeof ReactLeaflet.Polygon;
+  useMapEvents: typeof ReactLeaflet.useMapEvents;
 };
+
+export function MyMap() {
+  const [leaflet, setLeaflet] = React.useState<null | Leaflet>(null);
+  useEffect(() => {
+    const loadLeaflet = async () => {
+      const lazyLoadedLeaflet = await import("react-leaflet");
+      setLeaflet({
+        MapContainer: lazyLoadedLeaflet.MapContainer,
+        TileLayer: lazyLoadedLeaflet.TileLayer,
+        Circle: lazyLoadedLeaflet.Circle,
+        Polygon: lazyLoadedLeaflet.Polygon,
+        useMapEvents: lazyLoadedLeaflet.useMapEvents,
+        Marker: lazyLoadedLeaflet.Marker,
+      });
+    };
+    loadLeaflet();
+  }, []);
+
+  if (!leaflet) {
+    return <div>Loading...</div>;
+  }
+  const { MapContainer, TileLayer, Marker } = leaflet;
+
+  return (
+    <MapContainer
+      center={[37.7749, -122.4194]} // San Francisco
+      zoom={13}
+      style={{ height: "400px", width: "100%" }}
+      scrollWheelZoom={true}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; OpenStreetMap contributors"
+      />
+      <Marker position={[37.7749, -122.4194]}></Marker>
+    </MapContainer>
+  );
+}
--- pages/map/+Page.tsx
+++ pages/map/+Page.tsx
@@ -1,11 +1,44 @@
 import React from "react";
 import { MyMap } from "../../components/MyMap";
+import { css } from "@emotion/react";
 
 export default function MapPage() {
   return (
-    <>
-      <h1>My Map Page</h1>
-      <MyMap />
-    </>
+    <div css={styles.pageContainer}>
+      <h1>Select Map Areas</h1>
+      <div css={styles.mapContainer}>
+        <MyMap />
+      </div>
+      <div css={styles.buttonContainer}>
+        <button css={styles.button}>Button 1</button>
+        <button css={styles.button}>Button 2</button>
+      </div>
+    </div>
   );
 }
+
+const styles = {
+  pageContainer: css({
+    height: "100vh",
+    width: "100vw",
+  }),
+  mapContainer: css({
+    height: "400px",
+    width: "100%",
+    marginBottom: "20px",
+  }),
+  buttonContainer: css({
+    display: "flex",
+    flexDirection: "row",
+    marginTop: "20px",
+    gap: "10px",
+  }),
+  button: css({
+    padding: "10px 20px",
+    backgroundColor: "#007bff",
+    color: "#fff",
+    border: "none",
+    borderRadius: "5px",
+    cursor: "pointer",
+  }),
+};
